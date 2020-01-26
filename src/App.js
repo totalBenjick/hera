c83d4a36ce3515c5ea8367fdfc8955bdd7455e50
totalBenjick <imoiseslezama@gmail.com>
2020-01-25 20:55:16 -0600

    Avance react spring

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from "react";
-import Store from "./components/Store";
-import useDevice from "usedevice";
+import React from 'react';
+import Store from './components/Store';
+import useDevice from 'usedevice';
+import './App.css';
 
 const breakpoints = [
-  { name: "phone", min: 0, max: 766 },
-  { name: "tablet", min: 767, max: 1080 },
-  { name: "desktop", min: 1080, max: Infinity }
+	{ name: 'phone', min: 0, max: 766 },
+	{ name: 'tablet', min: 767, max: 1080 },
+	{ name: 'desktop', min: 1080, max: Infinity },
 ];
 
 function App() {
-  const device = useDevice({ breakpoints });
-  return <Store device={device} />;
+	const device = useDevice({ breakpoints });
+	return <Store device={device} />;
 }
 
 export default App;
--- src/components/Store/index.js
+++ src/components/Store/index.js
@@ -1,11 +1,33 @@
-import React from 'react';
-import styles from './index.module.sass';
+import React, { useState } from 'react';
+import Header from '../Header/index';
+import AutoCarrousel from '../AutoCarrousel/index';
+import img1 from '../../images/1_table.jpg';
+import img2 from '../../images/2_table.jpg';
+import img3 from '../../images/3_table.jpg';
+
+const items = [
+	{
+		image: img1,
+	},
+	{
+		image: img2,
+	},
+	{
+		image: img3,
+	},
+];
 
 function Store(device) {
+	const [index, setIndex] = useState(0);
+
 	return (
 		<>
-			<div className={`${styles.store} f1 w-100`}>Tixili</div>
-			<div>dsfd</div>
+			<Header />
+			<AutoCarrousel
+				items={items}
+				index={index}
+				setIndex={setIndex}
+			/>
 		</>
 	);
 }
